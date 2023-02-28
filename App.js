e230cb2b47d2a2da21abd5d759bc60bf052afb86
Dorjee <rishav345@gmail.com>
2023-02-27 19:26:41 -0500

    added some state, onChange,onPress and functions to listen to the event

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [goal, setGoal] = useState("");
+  const [goalsList, setgoalsList] = useState([])
+
+  function addinputHandler(text) {
+    setGoal(text);
+  }
+
+  function addgoalHandler() {
+   setgoalsList(prevList => [...goalsList, goal])
+  }
+
+  const displayItems = goalsList.map(goals => {
+    return (
+      <View key ={Math.random()}>
+           <Text>{goalsList.goals}</Text>
+      </View>
+     
+    )
+  })
+
+
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput styles={styles.textInput} placeholder="your course goal" />
-        <Button placeholder="Add Goal" />
+        <TextInput
+          styles={styles.textInput}
+          placeholder="your course goal"
+          onChangeText={addinputHandler}
+        />
+        <Button title="Add Goal" onPress={addgoalHandler} />
       </View>
       <View styles={styles.inputContainer}>
-        <Text>Goals.....</Text>
+        <Text>{displayItems}</Text>
       </View>
     </View>
   );
@@ -17,28 +44,26 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    paddingTop: 50,
-    paddingHorizontal:16
+    paddingTop: 1,
+    paddingHorizontal: 16,
   },
   inputContainer: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: 'grey',
-
+    borderBottomColor: "grey",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
     width: "70%",
     marginRight: 8,
-    padding:8
+    padding: 8,
   },
   goalsContainer: {
-    flex: 5, 
-
-  }
+    flex: 1,
+  },
 });
